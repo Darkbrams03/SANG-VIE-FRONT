6d5be6288f3b44b5d872df68a94ccf2955da45de
DARKBRAMS03 <erensmith2005@gmail.com>
2026-04-22 11:31:12 +0100

    Fix: Nettoyage structure App.jsx et imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import AdminDash from './pages/AdminDashboardjsx';
+import AdminDash from './pages/AdminDashboard';
 import AgentDashboard from './pages/AgentDashboard';
 
 function App() {
